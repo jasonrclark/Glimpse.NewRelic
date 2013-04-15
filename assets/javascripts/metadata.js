1124898e971d13278cc5098b4b36b4fa12acf4a7
Jason R. Clark <jclark@newrelic.com>
2013-04-15 12:57:50 -0700

    Don't bother with plugin config for now--we don't have docs yet anyway

--- assets/javascripts/metadata.js
+++ assets/javascripts/metadata.js
@@ -1,22 +1,4 @@
 glimpse.data.initMetadata({
-    "plugins": {
-        "glimpse_configuration": {
-            "documentationUri": "http://getglimpse.com/Help/Configuration-Tab", 
-            "layout": null
-        }, 
-        "glimpse_environment": {
-            "documentationUri": "http://getglimpse.com/Help/Environment-Tab", 
-            "layout": null
-        }, 
-        "glimpse_request": {
-            "documentationUri": "http://getglimpse.com/Help/Request-Tab", 
-            "layout": null
-        }, 
-        "glimpse_server": {
-            "documentationUri": "http://getglimpse.com/Help/Server-Tab", 
-            "layout": null
-        } 
-    }, 
     "resources": {
         "glimpse_client": "/glimpse/assets/javascripts/client.js", 
         "glimpse_metadata": "/glimpse/assets/javascripts/metadata.js", 
