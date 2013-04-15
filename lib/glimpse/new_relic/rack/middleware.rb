e427ced4de418a148417a2207b0167043ef93eb7
Ben Weintraub <ben@newrelic.com>
2013-04-15 10:40:07 -0700

    Adding middleware code to inject JS into HTML response bodies

--- lib/glimpse/new_relic/rack/middleware.rb
+++ lib/glimpse/new_relic/rack/middleware.rb
@@ -3,13 +3,58 @@ require 'rack'
 module Glimpse::NewRelic
   module Rack
     class Middleware
+      attr_reader :log
+
       def initialize(app, options = {})
         @app = app
+        @log = Logger.new(STDERR)
       end
 
       # method required by Rack interface
       def call(env)
-        @app.call(env)   # [status, headers, response]
+        status, headers, response = @app.call(env)
+        if should_inject_client?(status, headers)
+          original_body = read_response_body(response)
+          instrumented_body = inject_javascript_tag(original_body, headers, :src => "glimpse/client")
+          response = ::Rack::Response.new(instrumented_body, status, headers)
+          response.finish
+        end
+        [status, headers, response]
+      end
+
+      def should_inject_client?(status, headers)
+        status == 200 && headers["Content-Type"] && headers["Content-Type"].include?("text/html") &&
+          !headers['Content-Disposition'].to_s.include?('attachment')
+      end
+
+      def read_response_body(response)
+        body = ''
+        response.each { |chunk| body << chunk.to_s }
+        response.close if response.respond_to?(:close)
+        body
+      end
+
+      def inject_javascript_tag(rsp, headers, attrs)
+        if rsp.index("<body") && (body_close = rsp.rindex("</body>"))
+          tag = build_javascript_tag(attrs)
+          log.debug("Injecting tag '#{tag}' into body @ #{body_close}")
+          rsp = rsp[0..(body_close-1)] << tag << rsp[body_close..-1]
+          headers['Content-Length'] = rsp.length.to_s if headers['Content-Length']
+        else
+          log.warn("Did not find body tags to inject into")
+        end
+        rsp
+      end
+
+      def build_javascript_tag(attrs)
+        tag = "<script "
+        attr_strings = []
+        attrs.each do |key, val|
+          attr_strings << "#{key}=\"#{val}\""
+        end
+        tag << attr_strings.join(" ")
+        tag << "></script>"
+        tag
       end
     end
   end
