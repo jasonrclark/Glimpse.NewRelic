68ba0e8ffb5125ebdb2ebabe4433f0e0d657b949
Ben Weintraub <ben@newrelic.com>
2013-04-15 15:42:15 -0700

    Adding forgotten base.rb file

---
+++ lib/glimpse/new_relic/providers/base.rb
@@ -0,0 +1,21 @@
+module Glimpse
+  module NewRelic
+    module Providers
+      class Base
+        def initialize
+        end
+
+        def name
+          self.class.name.split("::").last
+        end
+
+        def notice_request(env, request_uuid, status, headers, response)
+        end
+
+        def data_for_request
+          {}
+        end
+      end
+    end
+  end
+end
