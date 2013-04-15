71daf5d4547c4ce1e5bb6e1ceafe1bb9c1bd6d15
Jason R. Clark <jclark@newrelic.com>
2013-04-15 10:29:22 -0700

    Automatic wire-up of middleware on Rails 3+

--- lib/glimpse/new_relic.rb
+++ lib/glimpse/new_relic.rb
@@ -3,6 +3,27 @@ require "glimpse/new_relic/rack/middleware"
 
 module Glimpse
   module NewRelic
-    # Your code goes here...
+    def self.startup
+      rails_config.middleware.use Glimpse::NewRelic::Rack::Middleware unless rails_config.nil?
+    end
+
+    def self.rails_config
+      if defined?(::Rails) && ::Rails.respond_to?(:configuration)
+        ::Rails.configuration
+      else
+        puts "No Rails? Gotta install your own Glimpse::NewRelic::Rack::Middleware :("
+        nil
+      end
+    end
+  end
+end
+
+if Rails::VERSION::MAJOR.to_i >= 3
+  module NewRelic
+    class Railtie < Rails::Railtie
+      initializer "glimpse-new_relic.init" do |app|
+        Glimpse::NewRelic.startup
+      end
+    end
   end
 end
