d2d2c6b53be0b60e7052180b9748b6dda5bb0adf
Ben Weintraub <ben@newrelic.com>
2013-04-16 11:54:16 -0700

    Adding a middlewares provider

---
+++ lib/glimpse/new_relic/providers/middlewares.rb
@@ -0,0 +1,24 @@
+require 'new_relic/agent'
+
+module Glimpse
+  module NewRelic
+    module Providers
+      class Middlewares < Base
+        def data_for_request(_, request_info)
+          stack = Rails.configuration.middleware
+          rows = stack.map do |middleware|
+            args = middleware.args
+            [middleware.name, args.empty? ? nil : args]
+          end
+          rows.unshift(['Class', 'Args'])
+
+          request_info['data'][self.name] = {
+            'data' => rows,
+            'name' => 'Middleware Stack'
+          }
+        end
+      end
+    end
+  end
+end
+
