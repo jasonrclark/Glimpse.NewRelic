58156d0639e8d7f02cb6dd7a96de97cb5f1174ff
Jason R. Clark <jclark@newrelic.com>
2013-04-16 08:39:05 -0700

    Kill all whitespace

--- assets/javascripts/metadata.js
+++ assets/javascripts/metadata.js
@@ -1,16 +1,16 @@
 glimpse.data.initMetadata({
     "resources": {
-        "glimpse_client": "/glimpse/assets/javascripts/client.js", 
+        "glimpse_client": "/glimpse/assets/javascripts/client.js",
         "glimpse_logo": "/glimpse/assets/images/logo.png",
-        "glimpse_metadata": "/glimpse/assets/javascripts/metadata.js", 
-        "glimpse_popup": "/glimpse/popup?n=glimpse_popup&request_id={requestId}", 
-        "glimpse_sprite": "/glimpse/assets/images/sprite.png", 
+        "glimpse_metadata": "/glimpse/assets/javascripts/metadata.js",
+        "glimpse_popup": "/glimpse/popup?n=glimpse_popup&request_id={requestId}",
+        "glimpse_sprite": "/glimpse/assets/images/sprite.png",
 
         "glimpse_ajax": "/glimpse/ajax/NotImplemented",
-        "glimpse_config": "/glimpse/config/NotImplemented", 
-        "glimpse_history": "/glimpse/history/NotImplemented", 
-        "glimpse_request": "/glimpse/request/NotImplemented", 
+        "glimpse_config": "/glimpse/config/NotImplemented",
+        "glimpse_history": "/glimpse/history/NotImplemented",
+        "glimpse_request": "/glimpse/request/NotImplemented",
         "glimpse_version_check": "/glimpse/version_check/NotImplemented"
-    }, 
+    },
     "version": "1.2.0"
 });
