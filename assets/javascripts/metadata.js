64f504fdd7c666ae5fb2e1eac51387a152ac566d
Jason R. Clark <jclark@newrelic.com>
2013-04-16 08:38:16 -0700

    Adding logo endpoint

--- assets/javascripts/metadata.js
+++ assets/javascripts/metadata.js
@@ -1,14 +1,14 @@
 glimpse.data.initMetadata({
     "resources": {
         "glimpse_client": "/glimpse/assets/javascripts/client.js", 
-        "glimpse_popup": "/glimpse/popup?n=glimpse_popup&request_id={requestId}", 
+        "glimpse_logo": "/glimpse/assets/images/logo.png",
         "glimpse_metadata": "/glimpse/assets/javascripts/metadata.js", 
+        "glimpse_popup": "/glimpse/popup?n=glimpse_popup&request_id={requestId}", 
         "glimpse_sprite": "/glimpse/assets/images/sprite.png", 
 
         "glimpse_ajax": "/glimpse/ajax/NotImplemented",
         "glimpse_config": "/glimpse/config/NotImplemented", 
         "glimpse_history": "/glimpse/history/NotImplemented", 
-        "glimpse_logo": "/glimpse/logo/NotImplemented",
         "glimpse_request": "/glimpse/request/NotImplemented", 
         "glimpse_version_check": "/glimpse/version_check/NotImplemented"
     }, 
